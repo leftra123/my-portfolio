6e02f5de68464e18f0f7160c49481c734a164c64
master <eric.aguayo@inacapmail.cl>
2025-02-24 12:43:21 -0300

    Rename calendar navigation icons for clarity

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -54,9 +54,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconPrevious: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconNext: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
+      
       {...props}
     />
   );
